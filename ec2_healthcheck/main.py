8c19cead1e41225c405917204ea5b101bd9885fb
tonyrud <tonrudny@gmail.com>
2024-08-04 11:06:48 -0400

    cleanups

--- ec2_healthcheck/main.py
+++ ec2_healthcheck/main.py
@@ -1,8 +1,8 @@
 import boto3
 import schedule
 
-ec2_client = boto3.client('ec2', region_name="eu-west-3")
-ec2_resource = boto3.resource('ec2', region_name="eu-west-3")
+ec2_client = boto3.client('ec2', region_name="us-east-2")
+ec2_resource = boto3.resource('ec2', region_name="us-east-2")
 
 
 def check_instance_status():
@@ -10,6 +10,7 @@ def check_instance_status():
         IncludeAllInstances=True
     )
     for status in statuses['InstanceStatuses']:
+        print(status)
         ins_status = status['InstanceStatus']['Status']
         sys_status = status['SystemStatus']['Status']
         state = status['InstanceState']['Name']
