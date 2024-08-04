1872daa0d343a46b7630b829e4fd75f7a3bb9716
tonyrud <tonrudny@gmail.com>
2024-08-04 17:46:14 -0400

    backup volumes

---
+++ ec2_backup_volumes/main.py
@@ -0,0 +1,28 @@
+
+import boto3
+import schedule
+
+ec2_client = boto3.client('ec2', region_name="us-east-2")
+
+def create_volume_snapshots():
+    volumes = ec2_client.describe_volumes(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': ['prod']
+            }
+        ]
+    )
+
+    print(volumes['Volumes'])
+    for volume in volumes['Volumes']:
+        new_snapshot = ec2_client.create_snapshot(
+            VolumeId=volume['VolumeId']
+        )
+        print(new_snapshot)
+
+# create_volume_snapshots()
+schedule.every().day.do(create_volume_snapshots)
+
+while True:
+    schedule.run_pending()
